b8c4507d95b5a1d198cd69d2df84b1b88c5c71b3
Brandon Gonzales <bgonz12@hotmail.com>
2016-11-06 18:45:05 -0700

    Revert "Revert "Adding Game Porject (Attempt 2)""
    This reverts commit 12e64146fa55c45f21d2b3aa20bb198d33646d5f.

---
+++ LoopRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerController : MonoBehaviour {
+
+    private PlayerMovement playerMovement;
+
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+	void Update () {
+	    // Check for jump input
+        if( Input.GetButtonDown( "Jump" ) )
+        {
+            playerMovement.Jump();
+        }
+	}
+}
---
+++ LoopRunner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private BoxCollider2D col2D;
+    private Rigidbody2D rb2D;
+
+    [SerializeField] public LayerMask groundMask; // Used to check if the player is on the ground
+
+    [SerializeField] private float runSpeed;     // Player's constant run speed
+    [SerializeField] private float jumpStrength; // Player's jump strength
+
+    void Start()
+    {
+        // Set component references
+        col2D = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        UpdateRunning();
+    }
+
+    private void UpdateRunning()
+    {
+        // Store current velocity and update running velocity
+        Vector2 vel;
+        vel = rb2D.velocity;
+        vel.x = runSpeed;
+
+        // Set player rigidbody velocity
+        rb2D.velocity = vel;
+    }
+
+    public void Jump()
+    {
+        // Check if player is on the ground
+        RaycastHit2D _grounded = Physics2D.Raycast(
+            transform.position, 
+            Vector2.down, 
+            col2D.bounds.size.y / 2f + 0.05f,
+            groundMask
+        );
+
+        // Jump if player is on the ground
+        if( _grounded )
+        {
+            rb2D.AddForce( new Vector2( 0f, jumpStrength ) );
+        }
+    }
+}
