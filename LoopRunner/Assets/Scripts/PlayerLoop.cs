86af4a1da1e97923a32410f1273c92e54ffab1a9
Brandon Gonzales <bgonz12@hotmail.com>
2016-11-08 19:26:32 -0700

    Added Player Looping

---
+++ LoopRunner/Assets/Scripts/PlayerLoop.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerLoop : MonoBehaviour {
+
+    private float playerWidth;
+
+    void Start () {
+        playerWidth = GetComponent<BoxCollider2D>().bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        if( transform.position.x > Camera.main.orthographicSize * Camera.main.aspect + playerWidth/2f ) {
+            Vector3 newPosition = transform.position;
+
+            newPosition.x = -Camera.main.orthographicSize * Camera.main.aspect;
+
+            transform.position = newPosition;
+        } else if( transform.position.x < -Camera.main.orthographicSize * Camera.main.aspect - playerWidth/2f ) {
+            Vector3 newPosition = transform.position;
+
+            newPosition.x = Camera.main.orthographicSize * Camera.main.aspect;
+
+            transform.position = newPosition;
+        }
+	}
+}
